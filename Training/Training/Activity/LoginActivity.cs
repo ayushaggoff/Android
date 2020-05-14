9a3c7273d1f2c7a89dcef57b04ea37c9561a1a59
Ayush Aggarwal <ayusha@successive.com>
2020-05-14 19:30:49 +0530

    login Activity , login layout

---
+++ Training/Training/Activity/LoginActivity.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Training.Model;
+
+namespace Training.Activity
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class LoginActivity : AppCompatActivity
+    {
+        Button buttonSubmit;
+        Android.App.AlertDialog.Builder dialog;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.LoginLayout);
+            buttonSubmit = FindViewById<Button>(Resource.Id.buttonsubmit);
+            dialog = new Android.App.AlertDialog.Builder(this);
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            this.FindViewById<Button>(Resource.Id.buttonsubmit).Click += this.CheckValidation;
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            this.FindViewById<Button>(Resource.Id.buttonsubmit).Click -= this.CheckValidation;
+        }
+        public void CheckValidation(object sender, EventArgs e)
+        {
+            User userObj = new User();
+            EditText EditText_Email = FindViewById<EditText>(Resource.Id.editText_email);
+            EditText EditText_Password = FindViewById<EditText>(Resource.Id.editText_password);
+            userObj.Email = EditText_Email.Text.ToString();
+            userObj.Password = EditText_Password.Text.ToString();
+            Regex regex = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            bool isValid = regex.IsMatch(("" + userObj.Email).Trim());
+            Android.App.AlertDialog alert = dialog.Create();
+
+            if (userObj.Email == "" || userObj.Password == "")
+            {
+                alert.SetTitle("Alert");
+                alert.SetMessage("Enter All Fields");
+
+                alert.SetButton("OK", (c, ev) =>
+                {
+                    Toast.MakeText(this, "Enter All Fields", ToastLength.Long).Show();
+
+                });
+                alert.SetButton2("CANCEL", (c, ev) =>
+                {
+
+                });
+                alert.Show();
+            }
+            else if (!isValid)
+            {
+                alert.SetTitle("Alert");
+                alert.SetMessage("Enter Valid Email");
+
+                alert.SetButton("OK", (c, ev) =>
+                {
+                    Toast.MakeText(this, "Enter All Fields", ToastLength.Long).Show();
+
+                });
+                alert.SetButton2("CANCEL", (c, ev) =>
+                {
+
+                });
+                alert.Show();
+            }
+            else 
+            {
+                alert.SetTitle("Login");
+                alert.SetMessage("Success");
+
+                alert.SetButton("OK", (c, ev) =>
+                {
+                  
+
+                });
+                alert.SetButton2("CANCEL", (c, ev) =>
+                {
+
+                });
+                alert.Show();
+
+            }
+        }
+    }
+}
---
+++ Training/Training/obj/Debug/90/android/src/crc645aea788ebc5f01ab/LoginActivity.java
@@ -0,0 +1,65 @@
+package crc645aea788ebc5f01ab;
+
+
+public class LoginActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"";
+		mono.android.Runtime.register ("Training.Activity.LoginActivity, Training", LoginActivity.class, __md_methods);
+	}
+
+
+	public LoginActivity ()
+	{
+		super ();
+		if (getClass () == LoginActivity.class)
+			mono.android.TypeManager.Activate ("Training.Activity.LoginActivity, Training", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
