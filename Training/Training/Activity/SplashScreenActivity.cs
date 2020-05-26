95ecee51019e380bd0352b6f166e24d02462b451
Ayush Aggarwal <ayusha@successive.com>
2020-05-26 10:03:49 +0530

    request

--- Training/Training/Activity/SplashScreenActivity.cs
+++ Training/Training/Activity/SplashScreenActivity.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using Android;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -18,8 +20,10 @@ namespace Training.Activity
     [Activity(Label = "SplashScreenActivity", Theme="@style/Theme.Splash", NoHistory=true)]
     public class SplashScreenActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected async override void OnCreate(Bundle savedInstanceState)
         {
+                        await TryToGetPermission();
+
             base.OnCreate(savedInstanceState);
 //SetContentView(Resource.Layout.LoginLayout);
             Thread.Sleep(40);
@@ -63,5 +67,86 @@ namespace Training.Activity
                 }
             }
         }
+
+
+        #region permisson
+        async Task TryToGetPermission()
+        {
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                await GetPermission();
+                return;
+            }
+        }
+
+        const int RequestLocationId = 0;
+
+        readonly string[] PermissionGroupLocation =
+       {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
+        async Task GetPermission()
+        {
+            string permission = Manifest.Permission.AccessFineLocation;
+
+            if (CheckSelfPermission(permission) == (int)Android.Content.PM.Permission.Granted)
+            {
+                Toast.MakeText(this, "Loaction Permission Granted", ToastLength.Short).Show();
+                return;
+            }
+
+            if (ShouldShowRequestPermissionRationale(permission))
+            {
+                Android.App.AlertDialog.Builder alert = new Android.App.AlertDialog.Builder(this);
+                alert.SetTitle("Permissions Needed");
+                alert.SetMessage("The application need special permissions to continue");
+                alert.SetPositiveButton("Request Permissions", (senderAlert, args) =>
+                {
+                    RequestPermissions(PermissionGroupLocation, RequestLocationId);
+                });
+
+                alert.SetNegativeButton("Cancel", (senderAlert, args) =>
+                {
+                    Toast.MakeText(this, "Cancelled!", ToastLength.Short).Show();
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+
+
+                return;
+            }
+
+            RequestPermissions(PermissionGroupLocation, RequestLocationId);
+        }
+
+        public override async void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            switch (requestCode)
+            {
+                case RequestLocationId:
+                    {
+                        if (grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+                        {
+                            Toast.MakeText(this, "Special permissions granted", ToastLength.Short).Show();
+
+                        }
+                        else
+                        {
+
+                            Toast.MakeText(this, "Special permissions denied", ToastLength.Short).Show();
+
+                        }
+                    }
+                    break;
+            }
+
+        }
+
+        #endregion
+
+
     }
 }
