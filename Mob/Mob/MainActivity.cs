2bf7dfcd8a62576900f256e63fa9c56c4b452a32
Ayush Aggarwal <ayusha@successive.com>
2020-07-06 22:46:31 +0530

    broadcast receiver sms

---
+++ Mob/Mob/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Graphics;
+using Android.Gms.Vision.Faces;
+using Android.Gms.Vision;
+using Android.Util;
+using Android.Graphics.Drawables;
+
+namespace Mob
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        MyBroadcastReceiver broadcastReceiver;
+        ImageView imageView;
+        Button btnProcess;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            broadcastReceiver = new MyBroadcastReceiver();
+
+            imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+            btnProcess = FindViewById<Button>(Resource.Id.btnProcess);
+            
+           
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+            IntentFilter intentFilter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+            RegisterReceiver(broadcastReceiver, intentFilter);
+        }
+        protected override void OnStop()
+        {
+            base.OnStop();
+        //  UnregisterReceiver(broadcastReceiver);
+        }
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            UnregisterReceiver(broadcastReceiver);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Mob/Mob/obj/Debug/90/android/src/crc64bd5b877a3d6348d0/MyBroadcastReceiver.java
@@ -0,0 +1,47 @@
+package crc64bd5b877a3d6348d0;
+
+
+public class MyBroadcastReceiver
+	extends android.content.BroadcastReceiver
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onReceive:(Landroid/content/Context;Landroid/content/Intent;)V:GetOnReceive_Landroid_content_Context_Landroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Mob.MyBroadcastReceiver, Mob", MyBroadcastReceiver.class, __md_methods);
+	}
+
+
+	public MyBroadcastReceiver ()
+	{
+		super ();
+		if (getClass () == MyBroadcastReceiver.class)
+			mono.android.TypeManager.Activate ("Mob.MyBroadcastReceiver, Mob", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onReceive (android.content.Context p0, android.content.Intent p1)
+	{
+		n_onReceive (p0, p1);
+	}
+
+	private native void n_onReceive (android.content.Context p0, android.content.Intent p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
