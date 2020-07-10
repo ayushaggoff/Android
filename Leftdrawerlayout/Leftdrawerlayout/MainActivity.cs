c7c752eba7372375efc36dfafba5e25156bbfb84
Ayush Aggarwal <ayusha@successive.com>
2020-07-10 23:33:45 +0530

    Navigation Drawer

---
+++ Leftdrawerlayout/Leftdrawerlayout/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using Android.Support.Design.Widget;
+
+namespace Leftdrawerlayout
+{
+    [Activity(Label = "Leftdrawerlayout",Theme = "@style/Theme.DesignDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        DrawerLayout drawerLayout;
+       
+        NavigationView navigationView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(true);
+           SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+            // Get our button from the layout resource,
+            // and attach an event to it
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            if (navigationView != null)
+                setupDrawerContent(navigationView);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                    return true;
+            }
+            
+return base.OnOptionsItemSelected(item);
+        }
+        void setupDrawerContent(NavigationView navigationView)
+        {
+            navigationView.NavigationItemSelected += (sender, e) => {
+                e.MenuItem.SetChecked(true);
+                drawerLayout.CloseDrawers();
+            };
+        }
+    }
+}
+
